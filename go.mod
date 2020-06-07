53ae81fbf513a6ee25fb2af7b35c992866a17881
Daniel Zambrano <zambrano.capecci@gmail.com>
2020-06-07 15:59:14 -0400

    commit inicial

---
+++ go.mod
@@ -0,0 +1,10 @@
+module daniel.com/speechtotext
+
+go 1.14
+
+require (
+	github.com/IBM/go-sdk-core/v4 v4.0.4
+	github.com/gabriel-vasile/mimetype v1.1.0
+	github.com/joho/godotenv v1.3.0
+	github.com/watson-developer-cloud/go-sdk v1.6.0
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/IBM/go-sdk-core/v4/core"
+	"github.com/gabriel-vasile/mimetype"
+	"github.com/joho/godotenv"
+	"github.com/watson-developer-cloud/go-sdk/speechtotextv1"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	authenticator := &core.IamAuthenticator{
+		ApiKey: os.Getenv("API_KEY"),
+	}
+
+	options := &speechtotextv1.SpeechToTextV1Options{
+		Authenticator: authenticator,
+	}
+
+	speechToText, speechToTextErr := speechtotextv1.NewSpeechToTextV1(options)
+
+	if speechToTextErr != nil {
+		panic(speechToTextErr)
+	}
+
+	speechToText.SetServiceURL(os.Getenv("SERVICE_URL"))
+	model := "es-ES_BroadbandModel"
+
+	files := [1]string{"audioflac.flac"}
+	for _, file := range files {
+
+		mime, err := mimetype.DetectFile(file)
+		if err != nil {
+			panic(err)
+		}
+
+		var audioFile io.ReadCloser
+		var audioFileErr error
+		audioFile, audioFileErr = os.Open(file)
+		if audioFileErr != nil {
+			panic(audioFileErr)
+		}
+		result, _, responseErr := speechToText.Recognize(
+			&speechtotextv1.RecognizeOptions{
+				Audio:                     audioFile,
+				ContentType:               core.StringPtr(mime.String()),
+				Timestamps:                core.BoolPtr(true),
+				WordAlternativesThreshold: core.Float32Ptr(0.9),
+				Model:                     &model,
+			},
+			// Keywords:                  []string{"prueba", "sonido"},
+			// KeywordsThreshold:         core.Float32Ptr(0.5),
+		)
+		if responseErr != nil {
+			panic(responseErr)
+		}
+		//fmt.Println(response)
+		x := result.Results
+		for _, a := range x {
+			for _, t := range a.Alternatives {
+				fmt.Println(*t.Transcript)
+			}
+		}
+
+		//fmt.Println(x)
+		//b, _ := json.MarshalIndent(result, "", "  ")
+		//['results'][0]['alternatives'][0]['transcript']
+		//fmt.Println(string(b))
+	}
+}
